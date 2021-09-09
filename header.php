2cc03714a6a8ad0335c32e827e272ee872a7bbbb
Timothy Chin <73248416+zzSleepyCoderzz@users.noreply.github.com>
2021-09-10 00:03:20 +0800

    Color can now be maintained through pages

--- header.php
+++ header.php
@@ -2,58 +2,61 @@
 include 'sambung.php';
 ?>
 
- <!DOCTYPE html>
- <html lang="en" dir="ltr" style="padding: 0;">
-   <head >
-     <meta charset="utf-8">
-     <title>EZQUIZ</title>
-   </head>
-     <body  style="margin : 0;" >
-       <style media="screen">
-       #changecolor {
-         background: linear-gradient(to bottom right, pink, blue,lightblue);
-         box-shadow: none;
-         width: 150px;
-         height: 50px;
-         border-radius: 20px;
-         margin-top: 20px;
-         margin-right: 20px;
-         float: right;
-         width: 90px;
-         padding-left: 3px;
-         padding-right: 3px;
-         border : none;
-         color: white;
-         font-family: "Feather Bold";
-       }
-       #changecolor:hover{
-         box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
-         cursor: pointer;
-       }
+<!DOCTYPE html>
+<html lang="en" dir="ltr" style="padding: 0;">
 
-       #header1{
-         height:200px;
-         background-color: #32CD32;
-       }
+<head>
+  <meta charset="utf-8">
+  <title>EZQUIZ</title>
+</head>
 
-       #logo{
-         float: left;
-         margin-left: 20px;
-       }
+<body style="margin:0;" onload = "javascript:color();" >
+  <style media="screen">
+    #changecolor {
+      background: linear-gradient(to bottom right, pink, blue, lightblue);
+      box-shadow: none;
+      width: 150px;
+      height: 50px;
+      border-radius: 20px;
+      margin-top: 20px;
+      margin-right: 20px;
+      float: right;
+      width: 90px;
+      padding-left: 3px;
+      padding-right: 3px;
+      border: none;
+      color: white;
+      font-family: "Feather Bold";
+    }
 
-       </style>
+    #changecolor:hover {
+      box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
+      cursor: pointer;
+    }
 
-       <center id = "header1">
-         <button id = "changecolor"  onclick="color()"> Warna</button>
-         <br>
-         <img id = "logo" src="logo.png" alt="logo" width = "100px" height="60px">
-         <br>
-         <p>
-          <font style="margin-left:20px;" size = "+5" color = "white" font face = "Feather Bold" >
-            <?php echo $nama_sistem; ?>
-          </font>
-        </p>
-        <br>
-       </center>
-      </body>
- </html>
+    #header1 {
+      height: 200px;
+      background-color: #32CD32;
+    }
+
+    #logo {
+      float: left;
+      margin-left: 20px;
+    }
+  </style>
+
+  <center id="header1">
+      <button id="changecolor" onclick="color();increment();">Warna</button>
+      <br>
+      <img id="logo" src="logo.png" alt="logo" width="100px" height="60px">
+      <br>
+      <p>
+        <font style="margin-left:20px;" size="+5" color="white" font face="Feather Bold">
+          <?php echo $nama_sistem; ?>
+        </font>
+      </p>
+      <br>
+  </center>
+</body>
+
+</html>
--- sambung.php
+++ sambung.php
@@ -3,8 +3,8 @@ $host = "localhost";
 $user = "root";
 $password = "";
 $database = "ezquiz";
-$hubung = mysqli_connect ($host , $user, $password, $database );
-if (mysqli_connect_errno()){
+$hubung = mysqli_connect($host, $user, $password, $database);
+if (mysqli_connect_errno()) {
   echo "Proses sambung ke pangkalan data gagal";
   exit();
 }
@@ -13,31 +13,36 @@ $nama_sekolah = "SMK (L) METHODIST, JALAN HANG JEBAT";
 $nama_sistem = "ADVANCE-SISTEM PENILAIAN KENDIRI";
 $title = "EZQUIZ";
 $lencana = "lencana.jpg";
- ?>
+?>
 
- <script type="text/javascript">
- var num = 0;
- var newcolor = "#32CD32";
- var colors = ["#A9F8FB" , "#3DDC97" , "#B2FFA9" ,"#160F29" , "#020100" ,"#336699" , "#9EE493" , "#9046CF" ,"E8998D" , "#EED2CC" ,"#A1683A" ,"red" , "green" , "#F1D302" , "#32CD32"];
- function color(){
-   newcolor = colors[num];
-   document.getElementById('header1').style.backgroundColor = newcolor;
-   var x = document.getElementsByClassName( "menu1button" );
-   for( var i = 0; i < x.length; i++ ) {
-     x[i].style.backgroundColor= newcolor;
-   }
+<script type="text/javascript">
+  var colors = ["#A9F8FB", "#3DDC97", "#B2FFA9", "#160F29", "#020100", "#336699", "#9EE493", "#9046CF", "E8998D", "#EED2CC", "#A1683A", "red", "green", "#F1D302", "#32CD32"];
+  
 
-   var y = document.getElementsByClassName( "button" );
-   for( var i = 0; i < y.length; i++ ) {
-     y[i].style.backgroundColor= newcolor;
-   }
-
-
-    if(num >= colors.length-1){
-      num = 0;
+  function color() {
+    var newcolor = colors[localStorage.getItem("color")];
+    document.getElementById("header1").style.backgroundColor = newcolor;
+    var x = document.getElementsByClassName("menu1button");
+    for (var i = 0; i < x.length; i++) {
+      x[i].style.backgroundColor = newcolor;
     }
-    else{
-      num += 1;
+
+    var y = document.getElementsByClassName("button");
+    for (var i = 0; i < y.length; i++) {
+      y[i].style.backgroundColor = newcolor;
     }
- }
- </script>
+  }
+
+  function increment(){
+    if (localStorage.getItem("color") >= colors.length - 1) {
+      localStorage.setItem("color" , 0);
+    } else {
+      localStorage.setItem("color" , parseInt(localStorage.getItem("color"))+1);
+    }
+    location.reload();
+  }
+
+  function reset(){
+    localStorage.setItem("color", 0);
+  }
+</script>
